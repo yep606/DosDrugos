553ee4f25f3d0563b54c9ce6c557c730630f3771
Ilya Rogov <ilya.rogov.vladislav@gmail.com>
2024-09-27 16:46:16 +0300

    fix config

--- src/main/java/ru/rogoff/tlgbot/config/WebhookConfig.java
+++ src/main/java/ru/rogoff/tlgbot/config/WebhookConfig.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,18 +19,18 @@ public class WebhookConfig {
     @Value("${tlgbot.webhook.url}")
     private String webhookUrl;
 
-//    @Value("classpath:posheve.pem")
-//    private Resource resource;
+    @Value("classpath:posheve.pem")
+    private Resource resource;
 
     private final TelegramBot bot;
 
     @PostConstruct
     public void setWebhook() {
         try {
-//            byte[] bytes = FileCopyUtils.copyToByteArray(resource.getInputStream());
-//            log.info("Cert bytes: {}", bytes.length);
-//            SetWebhook request = new SetWebhook().url(webhookUrl).certificate(bytes);
-            SetWebhook request = new SetWebhook().url(webhookUrl);
+            byte[] bytes = FileCopyUtils.copyToByteArray(resource.getInputStream());
+            log.info("Cert bytes: {}", bytes.length);
+            SetWebhook request = new SetWebhook().url(webhookUrl).certificate(bytes);
+//            SetWebhook request = new SetWebhook().url(webhookUrl);
             boolean ok = bot.execute(request).isOk();
             if (ok) {
                 log.info("Webhook successfully set at URL: {}", webhookUrl);
