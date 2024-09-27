012f2faa455538458eb8e2dfb7cfe7dfbc257e4e
Ilya Rogov <ilya.rogov.vladislav@gmail.com>
2024-09-27 03:45:37 +0300

    fix deploy script, subshell doesn't see variables imported using source

--- deploy.sh
+++ deploy.sh
@@ -3,21 +3,23 @@
 # Load environment variables from the .env file
 if [ -f .env ]; then
   echo "Loading environment variables from .env file..."
-  source .env
+  export $(grep -v '^#' .env | xargs)  # Export all variables in the .env file
 else
   echo ".env file not found!"
   exit 1
 fi
 
 # Check if all required environment variables are set
-if [[ -z "$DB_USERNAME" || -z "$DB_PASSWORD" || -z "$TLGBOT_WEBHOOK_URL" || -z "$TLGBOT_BOT_TOKEN" ]]; then
+if [[ -z "$DB_USERNAME" || -z "$DB_PASSWORD" || -z "$WEBHOOK_URL" || -z "$BOT_TOKEN" ]]; then
   echo "Missing required environment variables!"
   exit 1
 fi
 
-# Build the project using Maven
-echo "Running 'mvn clean install'..."
-if ! mvn clean install; then
+# Build the project using Maven with environment variables passed as Maven properties
+echo "Running 'mvn clean install' with environment variables..."
+if ! mvn clean install \
+  -Dspring.profiles.active=prod;
+    then
   echo "Maven build failed!"
   exit 1
 fi
@@ -30,9 +32,7 @@ if [ -z "$JAR_FILE" ]; then
   exit 1
 fi
 
-# Run the Spring Boot application
+# Run the Spring Boot application with the active profile and pass environment variables
 echo "Running the Spring Boot application: $JAR_FILE"
-if ! java -jar "$JAR_FILE" --spring.profiles.active=prod; then
-  echo "Failed to run the Spring Boot application!"
-  exit 1
-fi
+java -Dspring.profiles.active=prod \
+  -jar "$JAR_FILE"
